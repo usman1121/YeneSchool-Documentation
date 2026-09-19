2e1c71405e8f15e008f82dd597ca74cd80a7b512
usman1121 <usmanabdella1121@gmail.com>
2026-09-19 23:19:14 +0300

    chore(release): finalize documentation portal styling, translations, and assets

---
+++ app.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import type { ContentNavigationItem, PageCollections } from '@nuxt/content'
+import * as nuxtUiLocales from '@nuxt/ui/locale'
+
+const appConfig = useAppConfig()
+const { seo } = appConfig
+useDocusShortcuts()
+const site = useSiteConfig()
+const { locale, locales, isEnabled, switchLocalePath } = useDocusI18n()
+const { isEnabled: isAssistantEnabled } = useAssistant()
+
+const nuxtUiLocale = computed(() => nuxtUiLocales[locale.value as keyof typeof nuxtUiLocales] || nuxtUiLocales.en)
+const lang = computed(() => nuxtUiLocale.value.code)
+const dir = computed(() => nuxtUiLocale.value.dir)
+const collectionName = computed(() => isEnabled.value ? `docs_${locale.value}` : 'docs')
+
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+  ],
+  htmlAttrs: {
+    lang,
+    dir,
+  },
+})
+
+useSeoMeta({
+  titleTemplate: seo.titleTemplate,
+  title: seo.title,
+  description: seo.description,
+  ogSiteName: site.name,
+  twitterCard: 'summary_large_image',
+})
+
+if (isEnabled.value) {
+  const route = useRoute()
+  const defaultLocale = useRuntimeConfig().public.i18n.defaultLocale!
+  onMounted(() => {
+    const currentLocale = route.path.split('/')[1]
+    if (!locales.some(locale => locale.code === currentLocale)) {
+      return navigateTo(switchLocalePath(defaultLocale) as string)
+    }
+  })
+}
+
+const { data: navigation } = await useAsyncData(() => `navigation_${collectionName.value}`, () => queryCollectionNavigation(collectionName.value as keyof PageCollections), {
+  watch: [locale],
+})
+
+provide('navigation', navigation)
+
+const { subNavigationMode } = useSubNavigation(navigation)
+</script>
+
+<template>
+  <UApp :locale="nuxtUiLocale">
+    <NuxtLoadingIndicator color="var(--ui-primary)" :height="3" />
+
+    <div class="flex">
+      <div
+        class="flex-1 min-w-0"
+        :class="{ 'docus-sub-header': subNavigationMode === 'header' }"
+      >
+        <AppHeader v-if="$route.meta.header !== false" />
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+        <AppFooter v-if="$route.meta.footer !== false" />
+
+        <ClientOnly>
+          <AppSearch :navigation="navigation" />
+          <LazyAssistantFloatingInput v-if="isAssistantEnabled" />
+        </ClientOnly>
+      </div>
+
+      <ClientOnly v-if="isAssistantEnabled">
+        <LazyAssistantPanel />
+      </ClientOnly>
+    </div>
+  </UApp>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .docus-sub-header {
+    --ui-header-height: 112px;
+  }
+}
+</style>
