53477f081319e7da709e9ad02938cd4aae05c612
usman1121 <usmanabdella1121@gmail.com>
2026-09-19 23:19:14 +0300

    feat(navigation): customize sidebar navigation tree component

---
+++ components/docs/DocsAsideLeftBody.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { sidebarNavigation } = useSubNavigation()
+const contentNavVariants = useUIConfig('contentNavigation')
+</script>
+
+<template>
+  <div class="sidebar-nav-wrapper py-2">
+    <UContentNavigation
+      :collapsible="true"
+      :highlight="contentNavVariants.highlight ?? true"
+      :highlight-color="contentNavVariants.highlightColor"
+      :variant="contentNavVariants.variant ?? 'link'"
+      :color="contentNavVariants.color"
+      :navigation="sidebarNavigation"
+      class="space-y-1"
+    />
+  </div>
+</template>
