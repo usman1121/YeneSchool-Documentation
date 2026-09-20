6753e62694e367209f0a7d4c39fe5e63c71706be
usman1121 <usmanabdella1121@gmail.com>
2026-09-20 16:51:30 +0300

    brand: chameleon front half facing left flush with Gradify wordmark

--- components/app/AppHeaderLogo.vue
+++ components/app/AppHeaderLogo.vue
@@ -9,13 +9,13 @@ const { hasLogo, headerLightUrl, headerDarkUrl, contextMenuItems } = useLogoAsse
       <div class="flex items-center">
         <img
           :src="headerLightUrl || '/logo-light.svg'"
-          :alt="appConfig.header?.logo?.alt || 'YeneSchool'"
+          :alt="appConfig.header?.logo?.alt || 'Gradify'"
           class="h-8 w-auto shrink-0 block dark:hidden"
           style="height: 32px; max-height: 32px; width: auto;"
         />
         <img
           :src="headerDarkUrl || '/logo-dark.svg'"
-          :alt="appConfig.header?.logo?.alt || 'YeneSchool'"
+          :alt="appConfig.header?.logo?.alt || 'Gradify'"
           class="h-8 w-auto shrink-0 hidden dark:block"
           style="height: 32px; max-height: 32px; width: auto;"
         />
